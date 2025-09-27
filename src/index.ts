350b896606fa27ddb645943d6536caeec55513ae
Ned Palacios <npdoesmc@gmail.com>
2025-09-27 12:36:41 +0800

    feat: export new evaluator and types modules

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 export * from './expression';
 export * from './validator';
+export * from './types';
+
+export {
+  RuleEvaluationError,
+  OperationError,
+  VariableError,
+  TableError,
+  RuleEvaluator,
+  ConditionalEvaluator,
+  conditionalEvaluator,
+  OPERATION_REGISTRY,
+} from './evaluator';
+
+export type { OperationFunction } from './evaluator';
