61e221b773a1c8d074557907c5efec2b14232344
Ned Palacios <npdoesmc@gmail.com>
2025-09-27 14:00:38 +0800

    fix: use default exports of evaluator

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
 export * from './expression';
 export * from './validator';
 export * from './types';
-
-export {
-  RuleEvaluationError,
-  OperationError,
-  VariableError,
-  TableError,
-  RuleEvaluator,
-  ConditionalEvaluator,
-  conditionalEvaluator,
-  OPERATION_REGISTRY,
-} from './evaluator';
-
-export type { OperationFunction } from './evaluator';
+export * from './evaluator';
